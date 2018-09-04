34816781b95fc5cb0462e74974c9d50722d83fdf
olafurj <olafurj@tern.is>
2018-09-04 10:12:55 +0000

    cache

---
+++ CMakeFiles/OpenWorld.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenWorld.dir/main.cpp.o"
+  "CMakeFiles/OpenWorld.dir/includes/glad/include/glad/glad.c.o"
+  "CMakeFiles/OpenWorld.dir/includes/stb_image/stb_image.cpp.o"
+  "CMakeFiles/OpenWorld.dir/includes/SOIL/SOIL.c.o"
+  "CMakeFiles/OpenWorld.dir/OpenEngine/Cubemap.cpp.o"
+  "CMakeFiles/OpenWorld.dir/OpenEngine/Texture.cpp.o"
+  "OpenWorld.pdb"
+  "OpenWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
